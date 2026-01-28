153f050b0edc3e9fb22307e23c606355e49e16c6
Toofy <mostafa.alsari@gmail.com>
2026-01-28 21:59:03 +0000

    fix icons
    This reverts commit 82887c2d98cec91ef16a53306fdcf0fdf8f26ecc.

--- modules/home-manager/ui/waybar.nix
+++ modules/home-manager/ui/waybar.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   theme = config.omarchy.activeTheme;
   cfg = config.omarchy.waybar;
@@ -19,34 +24,46 @@ in
 
     modules-right = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [ "tray" "bluetooth" "network" "pulseaudio" "battery" ];
+      default = [
+        "tray"
+        "bluetooth"
+        "network"
+        "pulseaudio"
+        "battery"
+      ];
       description = "Modules to display on the right side of waybar";
     };
   };
-
   config = {
     programs.waybar = {
       enable = true;
-      
+
       settings = {
         mainBar = {
           layer = "top";
           position = "top";
           height = 26;
           spacing = 0;
-          
+
           modules-left = cfg.modules-left;
           modules-center = cfg.modules-center;
           modules-right = cfg.modules-right;
-
           "hyprland/workspaces" = {
             format = "{icon}";
             on-click = "activate";
             format-icons = {
-              "1" = "1"; "2" = "2"; "3" = "3"; "4" = "4"; "5" = "5";
-              "6" = "6"; "7" = "7"; "8" = "8"; "9" = "9"; "10" = "0";
+              "1" = "1";
+              "2" = "2";
+              "3" = "3";
+              "4" = "4";
+              "5" = "5";
+              "6" = "6";
+              "7" = "7";
+              "8" = "8";
+              "9" = "9";
+              "10" = "0";
               active = "󱓻";
-              default = "";
+              default = "";
             };
           };
 
@@ -60,16 +77,26 @@ in
             format-wifi = "{icon}";
             format-ethernet = "󰀂";
             format-disconnected = "󰤮";
-            format-icons = ["󰤯" "󰤟" "󰤢" "󰤥" "󰤨"];
+            format-icons = [
+              "󰤯"
+              "󰤟"
+              "󰤢"
+              "󰤥"
+              "󰤨"
+            ];
             tooltip-format-wifi = "{essid} ({signalStrength}%)";
           };
 
           pulseaudio = {
             format = "{icon}";
-            format-muted = "";
+            format-muted = "";
             format-icons = {
-              headphone = "";
-              default = ["" "" ""];
+              headphone = "";
+              default = [
+                ""
+                ""
+                ""
+              ];
             };
             on-click = "pavucontrol";
           };
@@ -77,20 +104,41 @@ in
           battery = {
             format = "{capacity}% {icon}";
             format-icons = {
-              charging = ["󰢜" "󰂆" "󰂇" "󰂈" "󰢝" "󰂉" "󰢞" "󰂊" "󰂋" "󰂅"];
-              default = ["󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹"];
+              charging = [
+                "󰢜"
+                "󰂆"
+                "󰂇"
+                "󰂈"
+                "󰢝"
+                "󰂉"
+                "󰢞"
+                "󰂊"
+                "󰂋"
+                "󰂅"
+              ];
+              default = [
+                "󰁺"
+                "󰁻"
+                "󰁼"
+                "󰁽"
+                "󰁾"
+                "󰁿"
+                "󰂀"
+                "󰂁"
+                "󰂂"
+                "󰁹"
+              ];
             };
           };
-          
+
           bluetooth = {
-            format = "";
+            format = "";
             format-disabled = "󰂲";
             format-connected = "󰂱";
-            on-click = "blueman-manager"; 
+            on-click = "blueman-manager";
           };
         };
       };
-
       style = ''
         @define-color background ${theme.colors.background};
         @define-color foreground ${theme.colors.foreground};
@@ -131,12 +179,11 @@ in
 
         tooltip { padding: 2px; }
         #custom-update { font-size: 10px; }
-        
-        #clock { 
+        #clock {
           font-family: '${config.omarchy.font}';
-          margin-left: 8.75px; 
+          min-width: 150px;
+          margin-left: 8.75px;
         }
-        
         .hidden { opacity: 0; }
 
         #custom-screenrecording-indicator {
@@ -154,5 +201,6 @@ in
         #custom-voxtype.recording { color: #a55555; }
       '';
     };
+
   };
 }
