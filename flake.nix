676374b379b4f29456df6e5e90f2803b01aa1692
Toofy <mostafa.alsari@gmail.com>
2026-01-29 11:56:03 +0000

    Add elephant and walker dependencies

--- flake.nix
+++ flake.nix
@@ -10,15 +10,24 @@
     };
 
     hyprland.url = "github:hyprwm/Hyprland/v0.53.3";
-    
+
     # We keep this for future access to base16 schemes, but we won't use it for math.
     nix-colors.url = "github:misterio77/nix-colors";
 
     # LazyVim for NixOS
     lazyvim.url = "github:pfassina/lazyvim-nix/v15.13.0";
+    walker.url = "github:abenz1267/walker";
+    elephant.url = "github:abenz1267/elephant";
   };
 
-  outputs = { self, nixpkgs, home-manager, lazyvim, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      home-manager,
+      lazyvim,
+      ...
+    }@inputs:
     let
       system = "x86_64-linux";
       # We instantiate our custom lib here using nixpkgs lib
@@ -32,14 +41,21 @@
       nixosModules.default = import ./modules/nixos;
 
       # Home Manager module (user-level)
-      homeManagerModules.default = { config, pkgs, lib, ... }: {
-        imports = [ 
-          ./modules/home-manager
-          lazyvim.homeManagerModules.default
-        ];
-        # Pass our custom lib down to modules so we can access themes/utils
-        _module.args.omarchyLib = omarchyLib;
-      };
+      homeManagerModules.default =
+        {
+          config,
+          pkgs,
+          lib,
+          ...
+        }:
+        {
+          imports = [
+            ./modules/home-manager
+            lazyvim.homeManagerModules.default
+          ];
+          # Pass our custom lib down to modules so we can access themes/utils
+          _module.args.omarchyLib = omarchyLib;
+        };
 
       # Development/testing configuration
       nixosConfigurations.omanix-dev = nixpkgs.lib.nixosSystem {
