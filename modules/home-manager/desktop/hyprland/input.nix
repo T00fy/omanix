fae4e69e8f2834bd8cc7b45cb0a23b6e24b7502d
Toofy <mostafa.alsari@gmail.com>
2026-02-02 12:57:43 +0000

    keeps capslock working

--- modules/home-manager/desktop/hyprland/input.nix
+++ modules/home-manager/desktop/hyprland/input.nix
@@ -3,9 +3,9 @@
   wayland.windowManager.hyprland.settings = {
     input = {
       kb_layout = "us";
-      kb_options = "compose:caps";
+      kb_options = "compose:caps, caps:escape_shifted_capslock";
       follow_mouse = 1;
-      
+
       # touchpad = {
       #   natural_scroll = false;
       #   scroll_factor = 0.4;
