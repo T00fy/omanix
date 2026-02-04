7729882c56285ee2925c1daddebf4d20d5fc8729
Toofy <mostafa.alsari@gmail.com>
2026-02-04 00:29:29 +0000

    Try scale down steam

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -12,7 +12,18 @@ in
     programs = {
       steam = {
         enable = true;
-        remotePlay.openFirewall = true; # Open ports for Steam Remote Play
+        remotePlay.openFirewall = true;
+        
+        # --- ADD THIS SECTION ---
+        package = pkgs.steam.override {
+          extraEnv = {
+            # Reset GDK_SCALE to 1 so Steam doesn't double-scale itself
+            GDK_SCALE = "1"; 
+            # Optional: Force specific Steam scaling factor if 1 is too small
+            # STEAM_FORCE_DESKTOPUI_SCALING = "1.0"; 
+          };
+        };
+        # ------------------------
       };
       gamescope.enable = true;
       gamemode.enable = true;
