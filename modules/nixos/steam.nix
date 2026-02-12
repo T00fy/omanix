cf950c880d63e77b779d290c1646bfce9d5f5b84
Toofy <mostafa.alsari@gmail.com>
2026-02-12 23:48:46 +0000

    Attempt to fix weird steam issue

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -6,6 +6,26 @@
 }:
 let
   cfg = config.omanix;
+
+  # Wrap Steam to strip PrefersNonDefaultGPU and X-KDE-RunOnDiscreteGpu
+  # from its desktop entries. These properties cause Steam windows to fail
+  # to render when launched via app launchers (Walker/Wofi/Rofi) on
+  # Hyprland + XWayland. Terminal launches bypass the .desktop file and
+  # are unaffected.
+  # Ref: https://bbs.archlinux.org/viewtopic.php?id=300993
+  steamBase = pkgs.steam.override {
+    extraEnv = { };
+  };
+
+  steamPatched = steamBase.overrideAttrs (prev: {
+    postInstall = (prev.postInstall or "") + ''
+      find $out/share/applications -name '*.desktop' -exec \
+        sed -i \
+          -e '/^PrefersNonDefaultGPU/d' \
+          -e '/^X-KDE-RunOnDiscreteGpu/d' \
+          {} +
+    '';
+  });
 in
 {
   config = lib.mkIf (cfg.enable && cfg.steam.enable) {
@@ -14,21 +34,12 @@ in
       steam = {
         enable = true;
         remotePlay.openFirewall = true;
-
-        # Update the package override:
-        package = pkgs.steam.override {
-          extraEnv = {
-            # Tell Steam to scale its UI by 2x internally
-            #STEAM_FORCE_DESKTOPUI_SCALING = "2.0";
-
-            # Ensure GDK scale is consistent
-            #GDK_SCALE = "2";
-          };
-        };
+        package = steamPatched;
       };
       gamescope.enable = true;
       gamemode.enable = true;
     };
+
     environment.systemPackages = with pkgs; [
       mangohud
       protonup-qt
