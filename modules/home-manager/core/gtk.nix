4af5e88b63468d5e89cefd09e4d72a8abc11fa4b
Toofy <mostafa.alsari@gmail.com>
2026-08-14 01:17:47 +0100

    fix firefox reverting to light theme on update

--- modules/home-manager/core/gtk.nix
+++ modules/home-manager/core/gtk.nix
@@ -10,6 +10,14 @@ in
     liberation_ttf
   ];
 
+  # Declare a dark color-scheme preference. GTK apps read
+  # gtk-application-prefer-dark-theme, but xdg-desktop-portal-gtk reports
+  # org.freedesktop.appearance color-scheme from the dconf key below.
+  # Firefox 153+ (and other portal-aware apps) trust that portal signal
+  # rather than inferring dark from the "Adwaita-dark" theme name, so
+  # without this they render light.
+  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+
   gtk = {
     enable = true;
 
@@ -17,6 +25,9 @@ in
       name = "Adwaita-dark";
       package = pkgs.gnome-themes-extra;
     };
+
+    gtk3.extraConfig.gtk-application-prefer-dark-theme = true;
+    gtk4.extraConfig.gtk-application-prefer-dark-theme = true;
 
     iconTheme = {
       name = theme.meta.icon_theme;
